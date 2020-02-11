683a3a12bf5242af2c40fbda27a7874dadd2dc92
jomjom40110 <59276836+jomjom40110@users.noreply.github.com>
2020-02-11 17:56:37 +0700

    Update lab9_2.cpp

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,4 +1,18 @@
-//cout << "-------------------- SOTUS ---------------------";
-//cout << textline;
-//cout << "-------------------- SOTUS ---------------------";
-//Don't forget to change cout to output filestream
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+intmain(){
+	string textline;
+	ifstream source;
+	source.open("cheerbook.txt");
+	ofstream dest("cheerbook_copy.txt");
+	dest <<"-------------------- SOTUS --------------------"<< endl;
+	while(getline(source,textline)){
+		dest << textline <<'\n';	
+	}
+	dest <<"-------------------- SOTUS --------------------";
+	source.close();
+	return 0;
+}
